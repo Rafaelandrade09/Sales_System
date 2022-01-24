ef0205ba35fb2350eacef51973fdba64a8151cbc
Rafael Andrade <rafael.andrade09@outlook.com.br>
2022-01-24 12:12:38 -0300

    configuring the database for MySql

--- WebSalesSystem/Startup.cs
+++ WebSalesSystem/Startup.cs
@@ -3,23 +3,27 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using WebSalesSystem.Data;
+
 
 namespace WebSalesSystem
 {
     public class Startup
     {
+
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+
         public IConfiguration Configuration { get; }
+
+
+
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -27,8 +31,16 @@ namespace WebSalesSystem
             services.AddControllersWithViews();
 
             services.AddDbContext<WebSalesSystemContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("WebSalesSystemContext")));
+                    options.UseMySql(Configuration.GetConnectionString("WebSalesSystemContext"), builder =>
+                    builder.MigrationsAssembly("WebSalesSystem")));
         }
+
+
+
+
+
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
