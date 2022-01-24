2c00a174c6277614913e94710b1f00eeef0cd092
Rafael Andrade <rafael.andrade09@outlook.com.br>
2022-01-24 11:02:10 -0300

    Create  CRUD Scahffolding

---
+++ WebSalesSystem/Data/WebSalesSystemContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebSalesSystem.Models;
+
+namespace WebSalesSystem.Data
+{
+    public class WebSalesSystemContext : DbContext
+    {
+        public WebSalesSystemContext (DbContextOptions<WebSalesSystemContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Client> Client { get; set; }
+    }
+}
