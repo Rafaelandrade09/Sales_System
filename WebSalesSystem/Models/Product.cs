24ec1c364e189329041de68355e022f94f9023a6
Moacyr Pereira <moacyrpereira10@hotmail.com>
2022-01-24 15:40:46 -0300

    Criada class Base(determina a chave primaria dos produtos) e classe product criada

---
+++ WebSalesSystem/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebSalesSystem.Models
+{
+
+    [Table("Produtos")]
+    public class Product
+    {
+        [Column("Nome")]
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+
+        [Column("Descrição")]
+        [StringLength(500)]
+        public string Description { get; set; }
+
+
+        [Column("Preço")]
+        [Required]
+        [Range(1,100)]
+        public decimal Price { get; set; }
+
+
+        [Column("Imagem Url")]
+        [StringLength(300)]
+        public string ImageURL { get; set; }
+    }
+}
